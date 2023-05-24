33e6c684eab5a751d030e0e4235854270bf50cdd
strandgeek <strandgeek@gmail.com>
2023-05-23 23:27:45 -0400

    refactor: Refactor MainLayout component to accept children prop.
    - Refactor MainLayout component to accept children prop
    - Eliminate unused 'props' variable in MainLayout function
    - Render children in MainLayout component instead of hardcoded content

--- webapp/src/layouts/MainLayout/index.tsx
+++ webapp/src/layouts/MainLayout/index.tsx
@@ -2,13 +2,14 @@ import React, { FC } from 'react'
 import { Topbar } from './components/Topbar'
 
 export interface MainLayoutProps {
-  
+  children: React.ReactNode;
 }
 
-export const MainLayout: FC<MainLayoutProps> = (props) => {
+export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className="bg-slate-900 h-screen w-screen">
       <Topbar />
+      {children}
     </div>
   )
 }
