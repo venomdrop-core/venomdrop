f0e0e6f194d3f502aa643d797e80bb37b855396e
strandgeek <strandgeek@gmail.com>
2023-05-24 23:45:02 -0400

    feat: add CORS support to the API
    - Enable CORS support in the API

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('VenomDrop API Documentation')
