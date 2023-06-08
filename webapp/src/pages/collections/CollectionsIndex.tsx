d58f7391e9d6f8a60f7b1d7247b141b21543234c
strandgeek <strandgeek@gmail.com>
2023-06-08 00:16:07 -0400

    fix: collections not loading on CollectionsIndex page
    - Fix use of the fetched collections list
    - Add loading box component for collections loading
    - Use isLoading state from useCollections hook

--- webapp/src/pages/collections/CollectionsIndex.tsx
+++ webapp/src/pages/collections/CollectionsIndex.tsx
@@ -6,55 +6,58 @@ import { useCollections } from "../../hooks/useCollections";
 import { useVenomWallet } from "../../hooks/useVenomWallet";
 import { CollectionListingCard } from "../../components/CollectionListingCard";
 import { Collection } from "../../api/collections";
+import { LoadingBox } from "../../components/LoadingBox";
 
 export interface CollectionsIndexProps {}
 
 export const CollectionsIndex: FC<CollectionsIndexProps> = () => {
   const { address } = useVenomWallet();
-  const { data } = useCollections({ owner: address, publishStatus: ['PUBLISHED', 'DRAFT'] }, { limit: 50, skip: 0 }, { enabled: !!address });
+  const { data, isLoading } = useCollections({ owner: address, publishStatus: ['PUBLISHED', 'DRAFT'] }, { limit: 50, skip: 0 }, { enabled: !!address });
   const [showCreateModal, setShowCreateModal] = useState(false);
-  const collections = ((data as { collections?: Collection[] })?.collections || []);
+  const collections = ((data as Collection[]) || []);
   return (
     <MainLayout authRequired>
-      <div className="container mx-auto mt-24">
-        <div className="flex flex-col md:flex-row justify-between">
-          <div>
-            <h1 className="text-2xl">My Collections</h1>
-            <p className="mt-4 text-lg">
-              Create and manage VenomDrop collections
-            </p>
-          </div>
-          <div>
-            <button
-              className="btn btn-primary mt-4 md:mt-0"
-              onClick={() => setShowCreateModal(true)}
-            >
-              <PlusIcon className="w-5 h-5 mr-2" />
-              Create Collection
-            </button>
-          </div>
-        </div>
-
-        {collections && collections?.length > 0 ? (
-          <div className="mt-8">
-            <div className="grid grid-cols-4 gap-8">
-              {(collections || []).map((collection) => (
-                <div>
-                  <CollectionListingCard collection={collection} showPublishStatus linkToAdmin />
-                </div>
-              ))}
+      <LoadingBox loading={isLoading}>
+        <div className="container mx-auto mt-24">
+          <div className="flex flex-col md:flex-row justify-between">
+            <div>
+              <h1 className="text-2xl">My Collections</h1>
+              <p className="mt-4 text-lg">
+                Create and manage VenomDrop collections
+              </p>
+            </div>
+            <div>
+              <button
+                className="btn btn-primary mt-4 md:mt-0"
+                onClick={() => setShowCreateModal(true)}
+              >
+                <PlusIcon className="w-5 h-5 mr-2" />
+                Create Collection
+              </button>
             </div>
           </div>
-        ) : (
-          <div className="border border-dashed border-base-100 p-24 text-center rounded-md text-gray-500 mt-16">
-            You have no collections yet
-          </div>
-        )}
-      </div>
-      <CreateCollectionModal
-        open={showCreateModal}
-        setOpen={setShowCreateModal}
-      />
+
+          {collections && collections?.length > 0 ? (
+            <div className="mt-8">
+              <div className="grid grid-cols-4 gap-8">
+                {(collections || []).map((collection) => (
+                  <div>
+                    <CollectionListingCard collection={collection} showPublishStatus linkToAdmin />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div className="border border-dashed border-base-100 p-24 text-center rounded-md text-gray-500 mt-16">
+              You have no collections yet
+            </div>
+          )}
+        </div>
+        <CreateCollectionModal
+          open={showCreateModal}
+          setOpen={setShowCreateModal}
+        />
+      </LoadingBox>
     </MainLayout>
   );
 };
